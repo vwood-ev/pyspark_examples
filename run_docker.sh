1bf5a7a9bf66ab469a86f4566d15f671248f9b85
vwood-ev <105678621+vwood-ev@users.noreply.github.com>
2024-04-11 22:34:26 +1200

    Add new docker image as option to explore later

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker run -it --rm -p 8888:8888 -p 4040:4040 -p 4041:4041 -v $(pwd):/home/jovyan/work jupyter/pyspark-notebook
+
+
+# Updated quay version:
+# docker run -it --rm -p 8888:8888 -p 4040:4040 -p 4041:4041 -v $(pwd):/home/jovyan/work quay.io/jupyter/pyspark-notebook
